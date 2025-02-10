f5e05c3cf1c24058c8512a92fa3fab88c50e1bc2
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-02-10 18:13:34 +0600

    Navlink Implemented

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,12 @@
+import Blog from '@/components/Blog';
+import React from 'react';
+
+const BlogPage = () => {
+    return (
+        <div>
+           <Blog/>
+        </div>
+    );
+}
+
+export default BlogPage;
