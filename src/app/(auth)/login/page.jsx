6e6dec216f8f24e4ef8c6764b8140121eb700a0d
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-02-11 16:47:19 +0600

    singin and signup page is done

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,36 +1,67 @@
-import Image from 'next/image';
-import React from 'react';
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 
 const Page = () => {
-    return (
-        <div className='container mx-auto py-24 lg:px-24 '>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-12'>
-                <div>
-                    <Image src='/assets/images/login/login.svg' alt='Login image' height={540} width={540}/>
-                </div>
-                <div className='border-2 p-12 rounded-lg'>
-                    <h1 className='font-bold text-2xl text-center'>Login</h1>
-                    <form action="">
-                    <label htmlFor='email'>Email</label> <br />
-                    <input type="text" name='email' placeholder="Enter your email" className="input input-bordered w-full  mt-2" /><br /><br />
-                    <label htmlFor='email'>Password</label> <br />
-                    <input type="text" name='password' placeholder="Enter your password" className="input input-bordered w-full mt-2" /> <br /> <br />
-                    <button className='btn btn-primary w-full text-white'>Login</button>
-                    <h1 className='mt-4 text-center'>Or Sign with</h1>
-                    <div className='flex gap-4 justify-center mt-4'>
-                    <FaGoogle className='text-xl' />
-                    <FaFacebook className='text-xl'/>
-                    <FaLinkedinIn className='text-xl'/>
-                  
-                    </div>
-                    </form>
-                </div>
-            </div>
+    const handleLogin = async()=>{
+
+    }
+  return (
+    <div className="container mx-auto py-24 lg:px-24 ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+        <div>
+          <Image src="/assets/images/login/login.svg" alt="Login image" height={540} width={540} />
         </div>
-    );
-}
+        <div className="border-2 p-12 rounded-lg">
+          <h1 className="font-bold text-2xl text-center">Login</h1>
+          <form onSubmit={handleLogin} action="">
+            <label htmlFor="email">Email</label> <br />
+            <input type="text" name="email" placeholder="Enter your email" required className="input input-bordered w-full  mt-2" />
+            <br />
+            <br />
+            <label htmlFor="email">Password</label> <br />
+            <input
+              type="text"
+              name="password"
+              required
+              placeholder="Enter your password"
+              className="input input-bordered w-full mt-2"
+            />{" "}
+            <br /> <br />
+            <button type="submit" className="btn btn-primary w-full text-white">Login</button>
+            <h1 className="mt-4 text-center">Or Sign with</h1>
+            <div className="flex gap-4 justify-center mt-4">
+              <button>
+                <FaGoogle className="text-xl" />
+              </button>
+              <button>
+                <FaFacebook className="text-xl" />
+              </button>
+              <button>
+                <FaGithub className="text-xl" />
+              </button>
+              <button>
+                <FaLinkedinIn className="text-xl" />
+              </button>
+            </div>
+            <p className="text-center mt-4 text-sm">
+              Don't have an account?
+              <Link href={"/signup"} className="text-primary font-bold">
+                {" "}
+                Sign up
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Page;
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,19 +1,71 @@
-import Image from 'next/image';
-import React from 'react';
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 
 const Page = () => {
-    return (
-        <div className='container mx-auto'>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-12'>
-                <div>
-                    <Image src='/assets/images/login/login.svg'/>
-                </div>
-                <div>
-                    Form
-                </div>
-            </div>
+    const handleRegister = async()=>{
+
+    }
+  return (
+    <div className="container mx-auto py-24 lg:px-24 ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+        <div>
+          <Image src="/assets/images/login/login.svg" alt="Login image" height={540} width={540} />
         </div>
-    );
-}
+        <div className="border-2 p-12 rounded-lg">
+          <h1 className="font-bold text-2xl text-center">Sign Up</h1>
+          <form onSubmit={handleRegister} action="">
+            <label htmlFor="email">Name</label> <br />
+            <input type="text" name="name" placeholder="Enter your name" required className="input input-bordered w-full  mt-2" />
+            <br />
+            <br />
+            <label htmlFor="email">Email</label> <br />
+            <input type="text" name="email" placeholder="Enter your email" required className="input input-bordered w-full  mt-2" />
+            <br />
+            <br />
+            <label htmlFor="email">Password</label> <br />
+            <input
+              type="text"
+              name="password"
+              required
+              placeholder="Enter your password"
+              className="input input-bordered w-full mt-2"
+            />{" "}
+            <br /> <br />
+            <button type="submit" className="btn btn-primary w-full text-white">Sign Up</button>
+            <h1 className="mt-4 text-center">Or Sign with</h1>
+            <div className="flex gap-4 justify-center mt-4">
+              <button>
+                <FaGoogle className="text-xl" />
+              </button>
+              <button>
+                <FaFacebook className="text-xl" />
+              </button>
+              <button>
+                <FaGithub className="text-xl" />
+              </button>
+              <button>
+                <FaLinkedinIn className="text-xl" />
+              </button>
+            </div>
+            <p className="text-center mt-4 text-sm">
+              Already have an account?
+              <Link href={"/login"} className="text-primary font-bold">
+                {" "}
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Page;
