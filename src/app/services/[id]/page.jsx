ebfcf102e26723a4559f28db9df523c52d7d62b6
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-02-17 02:47:40 +0600

    Deteails page design

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,37 +1,213 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Page = () => {
-    return (
-        <div className='container mx-auto'>
-            <h1>Details page</h1>
-            {/*Banner Section*/}
-            <div className='bg-red-400 rounded-lg h-60'>
-                <h1 className='pt-24 px-24 font-bold text-white text-2xl lg:text-4xl'>Service Details</h1>
-                <div className='flex justify-center mt-20  bg-[#FF3811] rounded-t-full font-bold text-xl text-white w-56 mx-auto '>
-                <Link href={"#"}>Home/</Link>
-                <Link href={"#"}>Service Details</Link>
-                </div>
-            </div>
-            {/**Service Section */}
-            <div className='flex flex-col md:flex-row justify-between border-2 border-e-green-500'>
-                <div className='w-[1300px] border-2 border-red-500'>
-                    <Image className='rounded-lg' src='/assets/images/banner/4.jpg' alt='Image' width={1200} height={800}/>
-                </div>
-                <div className='w-[500px] border-2 border-orange-500'>
-                    <h1 className='font-bold text-xl ml-6'>Services</h1>
-                    <div className='flex flex-col gap-4 px-6'>
-                        <Link className='bg-gray-200 w-full px-4 py-2 rounded-lg' href={"#"}>Full Car Repair </Link>
-                        <Link className='bg-gray-200 w-full px-4 py-2 rounded-lg' href={"#"}>Engine Repair</Link>
-                        <Link className='bg-gray-200 w-full px-4 py-2 rounded-lg' href={"#"}>Automatic Services</Link>
-                        <Link className='bg-gray-200 w-full px-4 py-2 rounded-lg' href={"#"}>Engine Oil Change</Link>
-                        <Link className='bg-gray-200 w-full px-4 py-2 rounded-lg' href={"#"}>Battery Charge</Link>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container mx-auto">
+      {/*Banner Section*/}
+      <div className="bg-red-400 rounded-lg h-60">
+        <h1 className="pt-24 px-24 font-bold text-white text-2xl lg:text-4xl">Service Details</h1>
+        <div className="flex justify-center mt-20 lg:mt-16 p-1  bg-[#FF3811] rounded-t-full font-bold text-xl text-white w-56 mx-auto ">
+          <Link href={"/"}>Home/</Link>
+          <Link href={"#"}>Service Details</Link>
         </div>
-    );
-}
+      </div>
+      {/**Service Section */}
+      <div className="flex flex-col md:flex-row justify-between mt-24 gap-4">
+        <div className="lg:w-[1300px] h-96 ">
+          <Image
+            className="rounded-lg object-cover w-full h-full"
+            src="/assets/images/banner/4.jpg"
+            alt="Image"
+            width={800}
+            height={500}
+          />
+        </div>
+        <div className="w-[500px]">
+          <div className="flex flex-col gap-4 px-6 bg-gray-300 rounded-lg p-12">
+            <h1 className="font-bold text-xl ml-2">Services</h1>
+            <div className=" flex justify-between items-center bg-gray-200 w-full px-4 py-2 rounded-lg">
+              <div>Full Car Repair</div>
+              <div>
+                <Link href={"#"}>
+                  <Image src="/Vector.png" alt="Image" width={20} height={20} />
+                </Link>
+              </div>
+            </div>
+            <div className=" flex justify-between items-center bg-gray-200 w-full px-4 py-2 rounded-lg">
+              <div>Engine Repair</div>
+              <div>
+                <Link href={"#"}>
+                  <Image src="/Vector.png" alt="Image" width={20} height={20} />
+                </Link>
+              </div>
+            </div>
+            <div className=" flex justify-between items-center bg-gray-200 w-full px-4 py-2 rounded-lg">
+              <div>Automatic Services</div>
+              <div>
+                <Link href={"#"}>
+                  <Image src="/Vector.png" alt="Image" width={20} height={20} />
+                </Link>
+              </div>
+            </div>
+            <div className=" flex justify-between items-center bg-gray-200 w-full px-4 py-2 rounded-lg">
+              <div>Engine Oil Change</div>
+              <div>
+                <Link href={"#"}>
+                  <Image src="/Vector.png" alt="Image" width={20} height={20} />
+                </Link>
+              </div>
+            </div>
+            <div className=" flex justify-between items-center bg-gray-200 w-full px-4 py-2 rounded-lg">
+              <div>Battery Charge</div>
+              <div>
+                <Link href={"#"}>
+                  <Image src="/Vector.png" alt="Image" width={20} height={20} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*Engine Service*/}
+      <div>
+        <div className="flex flex-col md:flex-row justify-between gap-6">
+          <div className="lg:w-[80%] ">
+            <h1 className="px-4 text-justify">
+              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some
+              form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a
+              passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.{" "}
+            </h1>
+
+            <div className="grid lg:grid-cols-2 gap-2 mt-4">
+              {demoCard?.map((demo) => (
+                <div key={demo.name} className="card bg-orange-50 border-2 border-t-[#FF3811] w-96 shadow-xl">
+                  <div className="card-body">
+                    <h2>{demo.name}</h2>
+                    <p>{demo.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div>
+              <p className="mt-4 px-4 text-justify ">
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some
+                form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a
+                passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.{" "}
+              </p>
+            </div>
+          </div>
+          <div className="lg:w-[30%] bg-black h-56 rounded-lg mt-6">
+            <h1 className="font-bold text-white ml-6 mt-6 text-xl">Download</h1>
+            <div className="flex justify-between items-center px-8 text-white">
+              <div className="flex items-center gap-2">
+                <div>
+                  <Image src="/Vectors.png" alt="Image" width={20} height={20} />
+                </div>
+                <div>
+                  <h1>Our Brochure</h1>
+                  <h1>Download</h1>
+                </div>
+              </div>
+              <div>
+                <Link href={"#"}>
+                  <div className="bg-[#FF3811] rounded-lg p-2">
+                    <Image className="brightness-0 invert" src="/Vector.png" alt="Image" width={20} height={20} />
+                  </div>
+                </Link>
+              </div>
+            </div>
+            <div className="flex justify-between items-center px-8 text-white mt-8">
+              <div className="flex items-center gap-2">
+                <div>
+                  <Image src="/Vectors.png" alt="Image" width={20} height={20} />
+                </div>
+                <div>
+                  <h1>Our Brochure</h1>
+                  <h1>Download</h1>
+                </div>
+              </div>
+              <div>
+                <Link href={"#"}>
+                  <div className="bg-[#FF3811] rounded-lg p-2">
+                    <Image className="brightness-0 invert" src="/Vector.png" alt="Image" width={20} height={20} />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*Three Simple steps:*/}
+      <div className="flex  justify-between gap-4 lg:gap-32 mt-12">
+        <div className="lg:w-[70%]">
+          <h1 className="text-2xl font-bold px-2">3 Simple Steps to Process</h1>
+          <p className="text-justify px-2">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some
+            form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a
+            passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text
+          </p>
+          <div className="grid lg:grid-cols-3 gap-6 rounded-lg">
+            {demoNumber?.map((card, index) => (
+              <div key={card.number} className="bg-base-100 w-40 h-40 shadow-xl flex flex-col items-center justify-center  rounded-lg p-2 mx-auto">
+                <h1 className="bg-red-500 text-white font-bold text-lg rounded-full w-12 h-12 flex items-center justify-center">
+                  {String(index + 1).padStart(2, "0")}
+                </h1>
+                <p className="font-bold">{card.name}</p>
+                <p >{card.description.slice(0,20)}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="lg:w-[30%]">
+          <h1 className="font-bold lg:text-2xl text-black mb-2">Price $250.00</h1>
+          <button className="btn btn-primary text-white lg:w-full">Proceed Checkout</button>
+        </div>
+      </div>
+        <div className="mt-10 lg:w-[90%]">
+            <Image className="rounded-lg object-cover object-top h-96"
+            src="/assets/images/banner/2.jpg" alt="Banner" width={800} height={600} />
+          </div>
+    </div>
+  );
+};
 
 export default Page;
+
+const demoCard = [
+  {
+    name: "Nazrul Islam",
+    description: "A passionate web developer specializing in MERN stack.",
+  },
+  {
+    name: "Book Store",
+    description: "An online platform for buying and selling books.",
+  },
+  {
+    name: "Deep Learning Model",
+    description: "A hybrid intrusion detection system using CNN and RNN.",
+  },
+  {
+    name: "Graphic Design",
+    description: "Creating stunning visuals and brand identities.",
+  },
+];
+
+const demoNumber = [
+    {
+      name: "Step One",
+      number: "01",
+      description: "A passionate web developer specializing in MERN stack.",
+    },
+    {
+      name: "Step Two",
+      number: "02",
+      description: "An online platform for buying and selling books.",
+    },
+    {
+      name: "Step Three",
+      number: "03",
+      description: "A hybrid intrusion detection system using CNN and RNN.",
+    },
+  ];
