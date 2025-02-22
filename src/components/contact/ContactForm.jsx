4dcd4cc7696692875acddf21a3850a648a0927a4
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-02-22 13:32:48 +0600

    Banner Section of Contact page added

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const ContactForm = () => {
     return (
         <div>
-            
+            {/*Container Box*/}
+        <div className="absolute inset-0 flex justify-center items-center">
+          <div className=" w-96 h-44  flex flex-col justify-center items-center bg-black/30 border-[1px] shadow-xl border-white rounded-lg">
+            <h1 className="text-white text-4xl font-extrabold">CONTACT US</h1>
+            <p className="text-white mt-2 text-xl font-bold">Get in Touch for Car Services</p>
+          </div>
+        </div>
         </div>
     );
 }
