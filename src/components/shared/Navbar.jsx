e381c02594810bba77183577117abafdef2f4d1d
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-02-23 00:00:31 +0600

    Next.js Configured for image domain

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -7,50 +7,69 @@ import { IoSearchOutline } from "react-icons/io5";
 import { SlHandbag } from "react-icons/sl";
 const Navbar = () => {
   const pathName = usePathname();
- 
-      const session = useSession();
+
+  const session = useSession();
+  const {email, image} = session?.data?.user || {}
+  
+  console.log(email, image);
+
   return (
     <div className="bg-base-100 ">
       <div className="navbar container mx-auto">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-            </svg>
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              {links?.map((link) => (
+                <Link key={link.path} href={link.path}>
+                  {link.title}
+                </Link>
+              ))}
+            </ul>
           </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <Link href="/">
+            <Image className="hidden lg:block" src="/Group.svg" alt="Logo" width={100} height={60} />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex  ">
+          <ul className="menu menu-horizontal px-1 flex gap-4 text-xl font-bold ">
             {links?.map((link) => (
-              <Link key={link.path} href={link.path}>
+              <Link
+                className={` hover:text-primary duration-300 ${pathName == link.path ? "text-red-500" : ""}`}
+                key={link.path}
+                href={link.path}
+              >
                 {link.title}
               </Link>
             ))}
           </ul>
         </div>
-        <Link href='/'>
-        <Image className="hidden lg:block" src="/Group.svg" alt="Logo" width={100} height={60} />
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex  ">
-        <ul className="menu menu-horizontal px-1 flex gap-4 text-xl font-bold ">
-          {links?.map((link) => (
-            <Link className={` hover:text-primary duration-300 ${pathName == link.path ? "text-red-500" : ""}`} key={link.path} href={link.path}>
-              {link.title}
-            </Link>
-          ))}
-        </ul>
-      </div>
-      <div className="navbar-end ">
+        <div className="navbar-end ">
           <div className="flex space-x-3 items-center">
-          <SlHandbag className="text-xl" />
-        <IoSearchOutline className="text-xl" />
-        <button className="btn text-primary btn-outline">Appointment</button>
-        <Link href={'/login'}>
-        { session.data ? <button onClick={()=> signOut()} className="btn btn-primary text-white font-bold">Logout</button> : <button className="btn btn-primary text-white font-bold">Login</button>}
-        </Link>
+            <SlHandbag className="text-xl" />
+            <IoSearchOutline className="text-xl" />
+            <div className="avatar">
+              <div className="w-10 rounded-full">
+                <Image src={image} alt="profile_image" width={30} height={30} />
+              </div>
+            </div>
+            <button className="btn text-primary btn-outline">Appointment</button>
+            <Link href={"/login"}>
+              {session.data ? (
+                <button onClick={() => signOut()} className="btn btn-primary text-white font-bold">
+                  Logout
+                </button>
+              ) : (
+                <button className="btn btn-primary text-white font-bold">Login</button>
+              )}
+            </Link>
           </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
