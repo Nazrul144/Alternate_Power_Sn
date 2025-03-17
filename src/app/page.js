4ad3953991b43764e66fd3b037f6487f313eac6f
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-17 18:03:04 +0600

    Ready to deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 import HomePage from "@/components/HomePage/HomePage";
 export default function Home() {
   return (
-   <main>
-     <HomePage/>
-   </main>
+    <main>
+      <HomePage />
+    </main>
   );
 }
--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,21 @@
-import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export const middleware = async(request)=>{
-    const token = cookies(request).get('next-auth.session-token')
-    const pathname = request.nextUrl.pathname;
-    if(pathname.includes('api')){
-        return NextResponse.next();
-    }
-    if(!token){
-        return NextResponse.redirect(new URL(`/login?redirect=${pathname}`, request.url))
-    }
+export const middleware = async (request) => {
+  const token = cookies(request).get("__Secure-next-auth.session-token");
+  const pathname = request.nextUrl.pathname;
+  if (pathname.includes("api")) {
     return NextResponse.next();
-}
+  }
+  if (!token) {
+    return NextResponse.redirect(new URL(`/login?redirect=${pathname}`, request.url));
+  }
+  return NextResponse.next();
+};
 
 export const config = {
-    matcher: [
-        '/mybookings/:path*', '/services/:path*' //path* means: After mybooking all route will be protected 
-    ]
-}
+  matcher: [
+    "/mybookings/:path*",
+    "/services/:path*", //path* means: After mybooking all route will be protected
+  ],
+};
