45a6a894f70dde79cfd2793e7c8cafd0ff79dc4b
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-01 23:39:32 +0600

    shadaw added

--- src/components/HomePage/CustomerReviews .jsx
+++ src/components/HomePage/CustomerReviews .jsx
@@ -22,13 +22,13 @@ const CustomerReviews = () => {
       <div className="absolute top-[15%] lg:top-1/4 left-1/2 transform -translate-x-1/2 flex flex-col lg:flex-row gap-6 w-4/5 lg:w-2/3 space-y-6 lg:space-y-0 max-w-full">
 
         {/* First Card */}
-        <div className="bg-transparent border-[1px] border-orange-950 p-6 rounded-lg shadow-lg w-full lg:w-1/2 h-auto lg:h-60 flex flex-col items-center justify-center ">
+        <div className="bg-transparent border-[1px] border-orange-950 p-6 rounded-lg shadow-xl w-full lg:w-1/2 h-auto lg:h-60 flex flex-col items-center justify-center ">
           <h2 className="font-bold text-xl mb-2">Card Title 1</h2>
           <p className="text-gray-700 text-center">This is some information inside the card. You can put anything here.</p>
         </div>
 
         {/* Second Card */}
-        <div className="bg-transparent border-[1px] border-orange-950 p-6 rounded-lg shadow-lg w-full lg:w-1/2 h-auto lg:h-60 flex flex-col items-center justify-center">
+        <div className="bg-transparent border-[1px] border-orange-950 p-6 rounded-lg shadow-xl w-full lg:w-1/2 h-auto lg:h-60 flex flex-col items-center justify-center">
           <h2 className="font-bold text-xl mb-2">Card Title 2</h2>
           <p className="text-gray-700 text-center">This is some other information inside the second card. Customize it as needed.</p>
         </div>
