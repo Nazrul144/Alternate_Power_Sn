15a543ba6cfa75758c55a951d930d2be7736243e
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-03 06:36:10 +0600

    motion added in the about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,21 +9,21 @@ export const metadata = {
 
 const AboutPage = () => {
     return (
-        <motion.div
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-            viewport={{ amount: 0.3 }}
-        >
+        <div>
             <About />
-            <div className='bg-orange-600 px-2 lg:px-32 py-32'>
+            <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                viewport={{ amount: 0.3 }}
+                className='bg-orange-600 px-2 lg:px-32 py-32'>
                 <h1 className='text-center text-4xl text-white font-bold'>Do you have any issue?</h1>
                 <p className='text-center text-white mt-2'>We believe in client satisfaction and we deliver service that can successfully meet or exceed the prospects of the stakeholders.</p>
                 <div className='mx-auto text-center text-white border-[1px] rounded-lg lg:w-32 p-2 mt-8'>
                     <Link href={'/contact'}>Contact Us</Link>
                 </div>
-            </div>
-        </motion.div>
+            </motion.div>
+        </div>
     );
 }
 
